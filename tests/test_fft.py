f652e17a2ee44f2c56c48d4a4914befd56411d81
UADER-PEC <31574049+lu7did@users.noreply.github.com>
2025-10-25 08:15:02 -0300

    restart

--- tests/test_fft.py
+++
@@ -1,9 +0,0 @@
-# (c) Dr. Pedro E. Colla 2020-2025 (LU7DZ)
-from __future__ import annotations
-
-import pytest
-
-
-@pytest.mark.skip(reason="FFT module removed; analyzer now inlined in Pipeline")
-def test_fft_bins_count_and_energy() -> None:
-    assert True
